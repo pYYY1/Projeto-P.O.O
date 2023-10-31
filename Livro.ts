0deeab8e675b902958a024527e890cd9e9761756
EversonPrieto <eversonlemosprieto@outlook.com>
2023-10-31 16:29:06 -0300

    comit.teste

--- Livro.ts
+++ Livro.ts
@@ -5,3 +5,11 @@ class Livro {
     private categoria: string = '';
     private exemplaresDisponiveis: number = 0;
 }
+
+class teste {
+    private titulo: string = '';
+    private autor: string = '';
+    private codigoLivro: string = '';
+    private categoria: string = '';
+    private exemplaresDisponiveis: number = 0;
+}
