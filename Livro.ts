8caf1dc8459a860f6664e9de3272754015f41a32
Gustavo Py <gustavooliveira2001@live.com>
2023-10-31 16:30:07 -0300

    Tirando o teste dnv

--- Livro.ts
+++ Livro.ts
@@ -5,11 +5,3 @@ class Livro {
     private categoria: string = '';
     private exemplaresDisponiveis: number = 0;
 }
-
-class teste {
-    private titulo: string = '';
-    private autor: string = '';
-    private codigoLivro: string = '';
-    private categoria: string = '';
-    private exemplaresDisponiveis: number = 0;
-}
